ed37da1787244c25e479d46b9d3cdc33efdd51fb
Vincent Yang <mscsharp@gmail.com>
2024-01-07 16:06:26 +1100

    feat: add generic attribute example

---
+++ Src/Sandbox/CSharp11/GenericAttributesDemo.cs
@@ -0,0 +1,22 @@
+﻿namespace Sandbox;
+
+internal class GenericAttributesBeforeCsharp11Demo
+{
+    [CustomType(typeof(string))]
+    public string? Method()=>default;
+}
+
+public class CustomTypeAttribute : Attribute
+{
+    public CustomTypeAttribute(Type t) => ParamType = t;
+    
+    public Type ParamType{get;}
+}
+
+internal class GenericAttributes
+{
+    [Generic<string>()]
+    public string Method()=>default;
+}
+
+public class GenericAttribute<T>:Attribute{}
